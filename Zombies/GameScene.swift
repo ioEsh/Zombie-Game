f93b556358dc682bd12f83983114b07b78280bca
Grey Grissom <grey.grissom@aya.yale.edu>
2017-11-08 21:22:37 -0800

    Add assets, zombie creation, background, initial setup

--- Zombies/GameScene.swift
+++ Zombies/GameScene.swift
@@ -7,83 +7,68 @@
 //
 
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
+
+/// scene / 1 screen
+class GameScene: SKScene
+{
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var zombie: SKSpriteNode! = nil
+    var lastUpdateTime: TimeInterval = 0
+    var dt: TimeInterval = 0
+    let zombieMovePointsPerSecond: CGFloat = 480.0
+    var velocity = CGPoint.zero
     
+    
+    /// SpriteKit's equivalent method to `viewDidLoad` in UIKit
+    ///
+    /// - Parameter view: the main view of the SKScene - root node to the scene object
     override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
+        let background = SKSpriteNode(imageNamed: "background1")
+       
+        //needs to add background to a parent node to view
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        // the position of background as -1 to ensure its the base layer when rendering
+        background.zPosition = -1
+        addChild(background)
+        let mySize = background.size
+        print("Size: \(mySize)")
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // zombie
+        zombie = SKSpriteNode(imageNamed: "zombie1")
+        zombie.position = CGPoint(x: 400, y: 400)
+//        zombie.setScale(2.0)
+        addChild(zombie)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func move(sprite: SKSpriteNode, velocity: CGPoint)
+    {
+        //1
+        
+        let amountToMove = CGPoint(x: velocity.x * CGFloat(dt),
+                                   y: velocity.y * CGFloat(dt))
+        print("Amount to move: \(amountToMove)")
+        
+        //2
+        
+        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
+    }
+    
+    
+    
+    override func update(_ currentTime: TimeInterval) { .
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        print("\(dt * 1000) milliseconds since last update!")
+        move(sprite: zombie, velocity: CGPoint(x: zombieMovePointsPerSecond, y: 0))
     }
 }
