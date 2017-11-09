b34b8e0cac7c11cb50f039a540fb81b7f9926f49
Grey Grissom <grey.grissom@aya.yale.edu>
2017-11-08 22:05:38 -0800

    Manual Movement of Main Character; SKSPRITENODE

--- Zombies/GameScene.swift
+++ Zombies/GameScene.swift
@@ -17,9 +17,32 @@ class GameScene: SKScene
     var lastUpdateTime: TimeInterval = 0
     var dt: TimeInterval = 0
     let zombieMovePointsPerSecond: CGFloat = 480.0
+    let playableRect: CGRect
     var velocity = CGPoint.zero
     
+    override init(size: CGSize)
+    {
+        let maxAspectRatio: CGFloat = 2.3
+        let playableHeight = size.width /  maxAspectRatio
+       let playableMargin = (size.height-playableHeight)/2.0
+        playableRect = CGRect(x: 0,
+                              y: playableMargin,
+                              width: size.width,
+                              height: playableHeight)
+        super.init(size: size)
+    }
     
+    //helper method to visualize playable rectangle of screen
+    func debugDrawPlayableArea(){
+        let shape = SKShapeNode(rect: playableRect)
+        shape.strokeColor = SKColor.red
+        shape.lineWidth = 4.0
+        addChild(shape)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
     /// SpriteKit's equivalent method to `viewDidLoad` in UIKit
     ///
     /// - Parameter view: the main view of the SKScene - root node to the scene object
@@ -40,8 +63,9 @@ class GameScene: SKScene
         zombie = SKSpriteNode(imageNamed: "zombie1")
         zombie.position = CGPoint(x: 400, y: 400)
 //        zombie.setScale(2.0)
+        zombie.setScale(0.7)
         addChild(zombie)
-        
+        debugDrawPlayableArea()
         
     }
     
@@ -59,9 +83,22 @@ class GameScene: SKScene
         sprite.position = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
     }
     
+    func moveZombieToward(location: CGPoint)
+    {
+        let offset = CGPoint(x: location.x - zombie.position.x, y: location.y - zombie.position.y)
+        let length = sqrt(Double(offset.x * offset.x + offset.y * offset.y))
+        
+        let direction = CGPoint(x: offset.x / CGFloat(length),y: offset.y / CGFloat(length))
+        
+        velocity = CGPoint(x: direction.x * zombieMovePointsPerSecond, y: direction.y * zombieMovePointsPerSecond)
+    }
     
+    func rotate(sprite: SKSpriteNode, direction: CGPoint)
+    {
+        sprite.zRotation = atan2(direction.y, direction.x)
+    }
     
-    override func update(_ currentTime: TimeInterval) { .
+    override func update(_ currentTime: TimeInterval) {
         if lastUpdateTime > 0 {
             dt = currentTime - lastUpdateTime
         } else {
@@ -69,6 +106,53 @@ class GameScene: SKScene
         }
         lastUpdateTime = currentTime
         print("\(dt * 1000) milliseconds since last update!")
-        move(sprite: zombie, velocity: CGPoint(x: zombieMovePointsPerSecond, y: 0))
+        move(sprite: zombie, velocity: velocity)
+        boundsCheckZombie()
+        rotate(sprite: zombie, direction: velocity)
+    }
+    
+    func sceneTouched(touchLocation: CGPoint)
+    {
+        moveZombieToward(location: touchLocation)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let touchLocation = touch.location(in: self)
+        sceneTouched(touchLocation: touchLocation)
+    }
+    
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        let touchLocation = touch.location(in: self)
+        sceneTouched(touchLocation: touchLocation)
+    }
+    
+    
+    func boundsCheckZombie() {
+        let bottomLeft = CGPoint(x: 0, y: playableRect.minY)
+        let topRight = CGPoint(x: size.width, y: playableRect.maxY)
+        
+        if zombie.position.x <= bottomLeft.x {
+            zombie.position.x = bottomLeft.x
+            velocity.x = -velocity.x
+        }
+        
+        if zombie.position.x >= topRight.x {
+            zombie.position.x = topRight.x
+            velocity.x = -velocity.x
+        }
+        if zombie.position.y <= bottomLeft.y {
+            zombie.position.y = bottomLeft.y
+            velocity.y = -velocity.y
+        }
+        
+        if zombie.position.y >= topRight.y {
+            zombie.position.y = topRight.y
+            velocity.y = -velocity.y
+        }
+        
     }
 }
